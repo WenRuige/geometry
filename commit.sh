436a8ea4814339a453bd3c5104761fe15ee3d0a6
941137860@qq.com <941137860@qq.com>
2018-10-11 08:15:59 +0800

    add 自动执行注释

---
+++ commit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#自动提交master
+
+msg=$1
+if [ -n "$msg" ]; then
+   git add -A
+   git commit -m"${msg}"
+   git pull
+   git status
+   git push origin master
+   echo "完成add、commit、pull，别忘了push"
+else
+    echo "请添加注释再来一遍"
+fi
