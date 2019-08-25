4eb4501e8fc29cef49472532861b8af740af31b3
941137860@qq.com <941137860@qq.com>
2019-08-25 12:04:16 +0800

    add 可视化

---
+++ base/browser.go
@@ -0,0 +1,15 @@
+package base
+
+type browser interface {
+	Echo()
+}
+
+// 为浏览器生成可用的poi串
+func GeneratePoiForBrowser() {
+
+}
+
+// 为高德浏览器生成可用的poi串
+func GeneratePoiForGaoDeBrowser() {
+
+}
---
+++ base/openbrowser.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	"runtime"
+	"fmt"
+	"os/exec"
+)
+
+var commands = map[string]string{
+	"windows": "cmd /c start",
+	"darwin":  "open",
+	"linux":   "xdg-open",
+}
+
+var Version = "0.1.0"
+
+// Open calls the OS default program for uri
+func Open(uri string) error {
+	run, ok := commands[runtime.GOOS]
+	if !ok {
+		return fmt.Errorf("don't know how to open things on %s platform", runtime.GOOS)
+	}
+
+	cmd := exec.Command(run, uri)
+	return cmd.Start()
+}
+
+
+
+
+
